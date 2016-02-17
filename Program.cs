27ff8cce3cf566bfcc5bb0b42fadefae07e79171
Poyo <poyoarya@gmail.com>
2016-02-16 20:58:25 -0800

    support for commands and better handling of spaces again

--- Program.cs
+++ Program.cs
@@ -16,18 +16,28 @@ namespace MarkovioBot
 		public static string email;
 		public static string password;
 
+        public static ulong lastServer;
 		public static List<Tuple<ulong, List<string>>> servers;
 
-		public static void Main(string[] args)
+        public static bool shouldExit;
+
+
+        public static void Main(string[] args)
 		{
 			Console.Write("Email: ");
 			email = Console.ReadLine();
 			Console.Write("Password: ");
 			password = Console.ReadLine();
+            Console.WriteLine();
+            Console.Clear();
 
-			client.MessageReceived += OnMessageReceived;
 
-			servers = JsonConvert.DeserializeObject<List<Tuple<ulong, List<string>>>>(
+            client.MessageReceived += OnMessageReceived;
+            client.LoggedIn += OnLoggedIn;
+            client.GatewaySocket.Connected += OnConnected;
+            client.GatewaySocket.Disconnected += OnDisconnected;
+
+            servers = JsonConvert.DeserializeObject<List<Tuple<ulong, List<string>>>>(
 				File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\MarkovioBot\serverData.json")
 			);
 
@@ -38,13 +48,95 @@ namespace MarkovioBot
 
 			client.Connect(email, password);
 
-			while (Console.ReadKey(true).Key != ConsoleKey.Q) { }
-			Environment.Exit(0);
+			while (!shouldExit)
+            {
+                Console.Write(">");
+
+                string command = Console.ReadLine().Trim();
+
+                if (command.ToUpper().StartsWith("QUIT"))
+                {
+                    shouldExit = true;
+                } else if (command.ToUpper().StartsWith("PROB") || command.ToUpper().StartsWith("FULLPROB"))
+                {
+                    List<string> commandArgs = Regex.Replace(command, @" + ", " ").Split(' ','\n','\r').Skip(1).ToList();
+
+                    if (!commandArgs.Any())
+                    {
+                        Console.WriteLine("Not enough arguments.");
+                    }
+                    else
+                    {
+                        List<ulong> serversInList = servers.Select(t => t.Item1).ToList();
+                        MarkovChain<string> chain = new MarkovChain<string>(1);
+                        foreach (var server in servers) {
+                            Server currentServer = client.GetServer(server.Item1);
+
+                            foreach (var messageFromServer in servers[serversInList.IndexOf(server.Item1)].Item2)
+                            {
+                                chain.Add(messageFromServer.Split(' ', '\n', '\r'));
+                            }
+
+                            List<string> words = new List<string>();
+
+                            words.Add(commandArgs.First());
+
+                            try
+                            {
+                                float totalWeight = chain.GetNextStates(words).Sum(x => x.Value);
+
+                                Dictionary<string, int> intChain = chain.GetNextStates(words);
+                                Dictionary<string, float> finalChain = new Dictionary<string, float>();
+
+                                foreach (var word in intChain.ToArray())
+                                {
+                                    finalChain.Add(word.Key, word.Value / totalWeight);
+                                }
+
+                                Console.WriteLine(totalWeight + " total entries in " + currentServer.Name);
+
+                                if (command.ToUpper().StartsWith("PROB"))
+                                {
+                                    int count = finalChain.Count;
+                                    foreach (var prob in finalChain.OrderBy(key => key.Value).Reverse())
+                                    {
+                                        if (prob.Value <= 0.05 && finalChain.Count - count >= 10)
+                                        {
+                                            Console.WriteLine("...and " + count + " more.");
+                                            break;
+                                        }
+                                        Console.WriteLine(Math.Round((prob.Value * 100), 3) + "%: " + prob.Key);
+                                        count--;
+                                    }
+                                }
+                                else
+                                {
+                                    foreach (var prob in finalChain.OrderBy(key => key.Value).Reverse())
+                                    {
+                                        Console.WriteLine(Math.Round((prob.Value * 100), 3) + "%: " + prob.Key);
+                                    }
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("No data from " + currentServer.Name);
+                            }
+                        }
+                    }
+                } else
+                {
+                    Console.WriteLine("Unrecognized.");
+                }
+
+                Console.WriteLine();
+            }
 		}
 
 		private static void OnMessageReceived(object sender, MessageEventArgs e)
 		{
-			List<ulong> serversInList = servers.Select(t => t.Item1).ToList();
+            lastServer = e.Server.Id;
+
+            List<ulong> serversInList = servers.Select(t => t.Item1).ToList();
 
             string rawMessage = Regex.Replace(e.Message.RawText, @" + ", " ");
             string formattedMessage = Regex.Replace(e.Message.Text, @" + ", " ");
@@ -58,25 +150,30 @@ namespace MarkovioBot
                 MarkovChain<string> chain = new MarkovChain<string>(1);
                 foreach (var messageFromServer in servers[serversInList.IndexOf(e.Server.Id)].Item2)
                 {
-                    chain.Add(messageFromServer.Split(' '));
+                    chain.Add(messageFromServer.Split(' ','\n','\r'));
                 }
 
-                if (rawMessage.Split(' ').Length > 1)
+                if (rawMessage.Split(' ','\n','\r').Length > 1)
                 {
                     try
                     {
-                        int value;
                         List<string> words = new List<string>();
-                        words.Add(rawMessage.Split(' ').Skip(1).First());
+                        words.Add(rawMessage.Split(' ','\n','\r').Skip(1).First());
 
-                        e.Channel.SendMessage(rawMessage.Split(' ').Skip(1).First() + " " + string.Join(" ", chain.Chain(words, new Random())));
+                        string chained = rawMessage.Split(' ', '\n', '\r').Skip(1).First() + " " + string.Join(" ", chain.Chain(words, new Random()));
+
+                        e.Channel.SendMessage(chained);
+                        client.SetGame(chained);
                     }
                     catch (ArgumentException) { }
                 } else
                 {
                     try
                     {
-                        e.Channel.SendMessage(string.Join(" ", chain.Chain(new Random())));
+                        string chained = string.Join(" ", chain.Chain(new Random()));
+
+                        e.Channel.SendMessage(chained);
+                        client.SetGame(chained);
                     }
                     catch (ArgumentException) { }
                 }
@@ -96,6 +193,23 @@ namespace MarkovioBot
 				Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\MarkovioBot\serverData.json",
 				JsonConvert.SerializeObject(servers)
 			);
-		}
-	}
+        }
+
+        private static void OnLoggedIn(object sender, EventArgs e)
+        {
+            Console.WriteLine("Logged in as " + client.CurrentUser.Name);
+            Console.WriteLine();
+            Console.Write(">");
+        }
+
+        private static void OnConnected(object sender, EventArgs e)
+        {
+            Console.Title = "MarkovioBot - Connected";
+        }
+
+        private static void OnDisconnected(object sender, EventArgs e)
+        {
+            Console.Title = "MarkovioBot - Disconnected";
+        }
+    }
 }
