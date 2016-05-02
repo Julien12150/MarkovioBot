4b08287a5f3cd5b6f21b2d451a160f62a64d0bc8
Poyo <poyoarya@gmail.com>
2016-05-02 16:07:31 -0700

    rewrote pretty much the whole thing
    gonna add better help and user config soon
    
    :japanese_goblin:
    :necktie: :+1:

---
+++ DiscordServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Text.RegularExpressions;
+using Discord;
+using Discord.Audio;
+using Markov;
+using Newtonsoft.Json;
+using Discord.API.Client.Rest;
+
+namespace MarkovioBot {
+	class DiscordServer {
+		[JsonProperty("id")]
+		public ulong Id;
+
+		[JsonProperty("inputs")]
+		public List<String> Inputs;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -12,13 +12,21 @@ using Newtonsoft.Json;
 using Discord.API.Client.Rest;
 
 namespace MarkovioBot {
+	enum MessageType {
+		Info,
+		Warning,
+		Error
+	}
+
 	class Program {
 		public static DiscordClient client = new DiscordClient();
 
 		public static string appData;
 
 		public static string token;
-		public static List<Tuple<ulong, List<string>>> servers;
+
+		public static List<DiscordServer> servers;
+		public static List<DiscordUser> users;
 
 		public static SpeechSynthesizer synth;
 
@@ -27,12 +35,17 @@ namespace MarkovioBot {
 		public static void Main(string[] args) {
 			Console.Title = "MarkovioBot";
 
+			appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PoyoBots\MarkovioBot\";
+			if (!Directory.Exists(appData)) {
+				Directory.CreateDirectory(appData);
+			}
+
 			for (int i = 0; i < args.Length; i++) {
 				if (args[i].Equals("-token") && args[i + 1] != null) {
 					token = args[i + 1];
-                }
+				}
 			}
-			  
+
 			if (token == null) {
 				Console.Write("Token: ");
 				token = Console.ReadLine();
@@ -41,28 +54,26 @@ namespace MarkovioBot {
 			Console.Clear();
 
 			client.MessageReceived += OnMessageReceived;
-			client.LoggedIn += (s, e) => Log(String.Format("Logged in as {0}.", client.CurrentUser.Name));
+			client.LoggedIn += (s, e) => {
+				Log(String.Format("Logged in as {0}.", client.CurrentUser.Name));
+				Log(String.Format("Current voice is {0}.", synth.Voice.Name));
+			};
 			client.GatewaySocket.Connected += (s, e) => Log("Connected.");
-			client.GatewaySocket.Disconnected += (s, e) => Log("Disconnected.");
+			client.GatewaySocket.Disconnected += (s, e) => Log("Disconnected.", MessageType.Warning);
+			
+			servers = new List<DiscordServer>();
 
-			appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PoyoBots\MarkovioBot\";
-
-			servers = JsonConvert.DeserializeObject<List<Tuple<ulong, List<string>>>>(
-				File.ReadAllText(appData + "serverData.json")
-			);
-
-			if (servers == null) {
-				servers = new List<Tuple<ulong, List<string>>>();
+			foreach (String file in Directory.GetFiles(appData)) {
+				string fileText = File.ReadAllText(file);
+				DiscordServer deserializedServer = JsonConvert.DeserializeObject<DiscordServer>(fileText);
+				servers.Add(deserializedServer);
 			}
 
 			synth = new SpeechSynthesizer();
 			synth.Rate = 1;
-
 			try {
 				synth.SelectVoice("Microsoft Haruka Desktop");
 			} catch (Exception) { }
-
-
 			client.UsingAudio(x => {
 				x.Mode = AudioMode.Outgoing;
 				x.Bitrate = 64;
@@ -75,86 +86,83 @@ namespace MarkovioBot {
 		}
 
 		private static void OnMessageReceived(object sender, MessageEventArgs e) {
-			List<ulong> serversInList = servers.Select(t => t.Item1).ToList();
-
-			string rawMessage = Regex.Replace(e.Message.RawText, @" + ", " ");
-			string formattedMessage = Regex.Replace(e.Message.Text, @" + ", " ");
-
-			if (e.User.Id == client.CurrentUser.Id ||
-				e.Channel.IsPrivate) {
+			if (e.User.Id == client.CurrentUser.Id) {
 				return;
-			} else if (rawMessage.StartsWith("<@" + client.CurrentUser.Id + ">") &&
-				   (e.Channel.Id == 151532383663947777 ||
-				   e.Channel.Id == 153663183045787649)) {
-				MarkovChain<string> chain = new MarkovChain<string>(1);
-				foreach (var messageFromServer in servers[serversInList.IndexOf(e.Server.Id)].Item2) {
-					chain.Add(messageFromServer.Split(' ', '\n', '\r'));
-				}
+			}
 
-				if (rawMessage.Split(' ', '\n', '\r').Length > 1) {
-					try {
-						List<string> words = new List<string>();
-						words.Add(rawMessage.Split(' ', '\n', '\r').Skip(1).First());
-
-						string chained = rawMessage.Split(' ', '\n', '\r').Skip(1).First() + " " + string.Join(" ", chain.Chain(words, new Random()));
-
-						SendMessage(chained, e);
-					} catch (ArgumentException) { }
-				} else {
-					try {
-						string chained = string.Join(" ", chain.Chain(new Random()));
-
-						SendMessage(chained, e);
-					} catch (ArgumentException) { }
-				}
+			if (e.Channel.IsPrivate) {
+				e.Channel.SendMessage("I'm MarkovioBot. I'm the digital manifestation of a dumpster. :put_litter_in_its_place:");
+				return;
 			} else {
-				if (!rawMessage.Contains("``") &&
-					!rawMessage.StartsWith("<@") &&
-					formattedMessage.Trim() != String.Empty &&
-					e.Channel.Id != 151532383663947777 &&
-					e.User.Id != 151545405614587904) {
-					if (!serversInList.Contains(e.Server.Id)) {
-						servers.Add(new Tuple<ulong, List<string>>(e.Server.Id, new List<string>()));
-					}
-					servers[serversInList.IndexOf(e.Server.Id)].Item2.Add(e.Message.Text);
+				if (!servers.Where(x => x.Id == e.Server.Id).Any()) {
+					servers.Add(new DiscordServer {
+						Id = e.Server.Id,
+						Inputs = new List<string>()
+					});
 				}
 			}
 
-			File.WriteAllText(
-				appData + "serverData.json",
-				JsonConvert.SerializeObject(servers)
-			);
+			if (!e.Message.Channel.Topic.ToUpper().Contains("[MARKOVIONOREAD]")) {
+				servers.Where(x => x.Id == e.Server.Id).First()
+					.Inputs
+					.Add(e.Message.Text);
+			}
+
+			if (e.Message.RawText.StartsWith("<@" + client.CurrentUser.Id + ">")) {
+				if (!e.Message.Channel.Topic.ToUpper().Contains("[MARKOVIONOSPEAK]")) {
+					MarkovChain<string> chain = new MarkovChain<string>(1);
+
+					foreach (string input in servers.Where(x => x.Id == e.Server.Id).First().Inputs) {
+						chain.Add(
+							Regex.Replace(input, @"\s+", " ").Split(' ')
+						);
+					}
+
+					SendMessage(String.Join(" ", chain.Chain()), e);
+				}
+			}
+
+			foreach (DiscordServer server in servers) {
+				File.WriteAllText(
+					appData + "/" + server.Id + ".json",
+					JsonConvert.SerializeObject(server)
+				);
+			}
 		}
 
 		private static async void SendMessage(string message, MessageEventArgs e) {
 			await e.Channel.SendMessage(message);
 
-			if (e.User.VoiceChannel != null) {
-				try {
-					IAudioClient audioClient = await e.User.VoiceChannel.JoinAudio();
+			try {
+				IAudioClient audioClient = await e.User.VoiceChannel.JoinAudio();
 
-					MemoryStream memoryStream = new MemoryStream();
+				MemoryStream memoryStream = new MemoryStream();
 
-					synth.SetOutputToAudioStream(memoryStream,
-						new SpeechAudioFormatInfo(48000, AudioBitsPerSample.Sixteen, AudioChannel.Stereo));
+				synth.SetOutputToAudioStream(memoryStream,
+					new SpeechAudioFormatInfo(48000, AudioBitsPerSample.Sixteen, AudioChannel.Stereo));
 
-					synth.Speak(message);
+				synth.Speak(message);
 
-					memoryStream.Position = 0;
-					memoryStream.CopyTo(audioClient.OutputStream);
-					memoryStream.Dispose();
-				} catch { }
+				memoryStream.Position = 0;
+				memoryStream.CopyTo(audioClient.OutputStream);
+				memoryStream.Dispose();
+
+			} catch (Exception error) {
+				Log(error.Message, MessageType.Error);
 			}
 		}
 
-		private static void Log(object message, bool warning = false) {
+		private static void Log(object message, MessageType type = MessageType.Info) {
 			DateTime time = DateTime.Now;
-			if (warning) {
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.Write("[WARN ");
-			} else {
+			if (type == MessageType.Info) {
 				Console.ForegroundColor = ConsoleColor.Green;
 				Console.Write("[INFO ");
+			} else if (type == MessageType.Warning) {
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.Write("[WARN ");
+			} else if (type == MessageType.Error) {
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.Write("[ERRR ");
 			}
 
 			Console.Write(time.ToString("HH:mm:ss dd/MM/yyyy"));
